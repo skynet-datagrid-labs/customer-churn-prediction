0d7faad43a33d3e75e4ef2b3ad9d781c35a67b33
Ŧ☉ПY KΣПGΔ <kengatony@gmail.com>
2026-04-19 10:33:18 +0300

    Update init.py

--- src/init.py
+++ src/init.py
@@ -1,28 +1,2 @@
-"""
-ML Company Workflow - Production ML Pipeline
-Author: Senior MLOps Engineer
-Version: 1.0.0
-"""
-
+# ML Company Workflow - Core processing modules
 __version__ = "1.0.0"
-__author__ = "ML Engineering Team"
-
-from .ingest import DataIngestor
-from .validate import DataValidator
-from .preprocess import DataPreprocessor
-from .feature_engineering import FeatureEngineer
-from .train import ModelTrainer
-from .evaluate import ModelEvaluator
-from .select_model import ModelSelector
-from .drift import DriftDetector
-
-__all__ = [
-    'DataIngestor',
-    'DataValidator', 
-    'DataPreprocessor',
-    'FeatureEngineer',
-    'ModelTrainer',
-    'ModelEvaluator',
-    'ModelSelector',
-    'DriftDetector'
-]
